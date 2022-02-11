d24f8c3cc9f7fb1c1e35812c2f8f27174af0d10a
Kandy Peter <kandypeter03@gmail.com>
2022-02-11 18:47:18 +0200

    display style

--- src/index.js
+++ src/index.js
@@ -1,39 +1,25 @@
 import './style.css';
 import createCards from './modules/render';
+import { getPokemonData, getPokemonNameUrl, asyncForEach } from './modules/getApiData';
 
 let pageNum = 1;
 let offsetPokemon = 0;
 const cardsForPage = 12;
 
-async function getPokemonNameUrl(offsetPokemon) {
-  const res = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?offset=" +
-      offsetPokemon +
-      "&limit=" +
-      cardsForPage
-  );
-  const data = await res.json();
-  return data;
+
+const loadingPage = () => {
+  let loading = document.createElement("h5");
+  loading.innerText = "Loading...";
+  loading.className = "loading";
+  cardContainer.appendChild(loading);
 }
 
-async function getPokemonData(url) {
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
-}
-
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
-
-function loadCards() {
+const loadCards = () => {
   cardContainer.innerHTML = "";
   loadingPage();
   let pokemon = [];
-  getPokemonNameUrl(offsetPokemon).then(async (nomeUrl) => {
-    await asyncForEach(nomeUrl.results, (el) =>
+  getPokemonNameUrl(offsetPokemon).then(async (nameUrl) => {
+    await asyncForEach(nameUrl.results, (el) =>
       getPokemonData(el.url).then((data) => {
         pokemon.push({
           name: data.name.charAt(0).toUpperCase() + el.name.substr(1),
@@ -47,11 +33,20 @@ function loadCards() {
   });
 }
 
-function loadingPage() {
-  let loading = document.createElement("h5");
-  loading.innerText = "Loading...";
-  loading.className = "loading";
-  cardContainer.appendChild(loading);
+const pageUp = () => {
+  if (pageNum < Math.ceil(1118 / cardsForPage)) {
+    pageNum++;
+    offsetPokemon = pageNum * cardsForPage;
+    loadCards();
+  }
+}
+
+const pageDown = () => {
+  if (pageNum > 0) {
+    pageNum--;
+    offsetPokemon = pageNum * cardsForPage;
+    loadCards();
+  }
 }
 
 const arrowRight = document.querySelector('*[data-target="arrowRight"]');
@@ -59,21 +54,5 @@ arrowRight.addEventListener("click", pageUp);
 
 const arrowLeft = document.querySelector('*[data-target="arrowLeft"]');
 arrowLeft.addEventListener("click", pageDown);
-
-function pageUp() {
-  if (pageNum < Math.ceil(964 / cardsForPage)) {
-    pageNum++;
-    offsetPokemon = pageNum * cardsForPage;
-    loadCards();
-  }
-}
-
-function pageDown() {
-  if (pageNum > 0) {
-    pageNum--;
-    offsetPokemon = pageNum * cardsForPage;
-    loadCards();
-  }
-}
 
 loadCards();
--- src/modules/render.js
+++ src/modules/render.js
@@ -2,15 +2,15 @@ const cardContainer = document.querySelector("#cardContainer");
 
 export default function createCards(pokemon) {
     const stringToInsert = pokemon.map((el) =>
-        `<div class="col-xl-3 col-lg-4 col-md-12 d-flex justify-content-around">
-    <div class="card my-4">
+        `
+    <div class="card card-${el.id}">
         <div class="card-body">
-            <img class="card-image" src="${el.image}" alt="Image Pokemon">
+            <img class="card-image" src="${el.image}" alt="Image ${el.name}">
             <h5 class="card-title">${el.id}. ${el.name}</h5>
             <p class="card-text">Type: ${el.type}</p>
         </div>
     </div>
-</div>`
+    `
     ).join("");
     cardContainer.innerHTML = stringToInsert;
 }; 
